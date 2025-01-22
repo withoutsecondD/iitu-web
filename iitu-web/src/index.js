5ea86c2be199655795b7662006d170b02333fb96
Diyar Z <marbiru15@gmail.com>
2025-01-22 09:48:51 +0500

    step 4

--- iitu-web/src/index.js
+++ iitu-web/src/index.js
@@ -1,18 +1,33 @@
-console.log('Hello, Diyar');
 import React from "react";
 import ReactDOM from 'react-dom';
+import {createRoot} from "react-dom/client";
 //const element = React.createElement('hi', null, 'Hello React World');
-const element = (
-    <div>
-        <h1>My Todo List</h1>
-        <input placeholder="search" />
+const ToDoList = () => {
+    return (
         <ul>
             <li>Install React</li>
             <li>Study React</li>
             <li>Use React</li>
             <li>Build React App</li>
         </ul>
-    </div>
-);
-ReactDOM.render(element,
-    document.getElementById('root'));
+    );
+};
+const AppHeader = () => {
+    return <h1>My Todo List</h1>
+}
+const SearchPanel = () => {
+    return <input placeholder="search"/>
+}
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <SearchPanel/>
+            <ToDoList />
+        </div>
+    )
+}
+
+// const element = <App />
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
