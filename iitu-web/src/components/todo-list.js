68d9723e9c0584ee4af5d89c7b0135cca6893317
Diyar Z <marbiru15@gmail.com>
2025-01-28 08:42:31 +0500

    step 7 - Using props in React

--- iitu-web/src/components/todo-list.js
+++ iitu-web/src/components/todo-list.js
@@ -4,8 +4,10 @@ import TodoListItem from "./todo-list-item";
 const TodoList = () => {
     return (
         <ul>
-            <li><TodoListItem/></li>
-            <li><TodoListItem/></li>
+            <li><TodoListItem label="Drink Coffee"/></li>
+            <li><TodoListItem label="Implement React Application"/></li>
+            <li><TodoListItem label="Make notes from your study"/></li>
+            <li><TodoListItem label="Do not drink alchohol" important/></li>
         </ul>
     );
 };
