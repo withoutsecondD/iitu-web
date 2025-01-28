ce073bb7ffdeabb26fc7aae364f0f8d408812233
Diyar Z <marbiru15@gmail.com>
2025-01-29 02:38:41 +0500

    step 22 - setState() modifications

--- iitu-web/src/components/todo-list-item/todo-list-item.js
+++ iitu-web/src/components/todo-list-item/todo-list-item.js
@@ -3,7 +3,8 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
     state = {
-        done: false
+        done: false,
+        important: false
     };
 
     onLabelClick = () => {
@@ -12,24 +13,35 @@ export default class TodoListItem extends Component {
         })
     };
 
+    onMarkImportant = () => {
+        this.setState( {
+            important: true
+        })
+    }
+
     render() {
-        const {label, important = false} = this.props;
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
+        const { label } = this.props;
+        const { done, important } = this.state;
+
+        let classNames = 'todo-list-item';
+        if (done) {
+            classNames += ' done';
+        }
+        if (important) {
+            classNames += ' important';
+        }
 
         return (
             <span className="todo-list-item">
               <span
                   className="todo-list-item-label"
-                  style={style}
-                  onclick={this.onLabelClick}>
+                  onClick={this.onLabelClick}>
                 {label}
               </span>
 
               <button type="button"
-                      className="btn btn-outline-success btn-sm float-right">
+                      className="btn btn-outline-success btn-sm float-right"
+                      onClick={ this.onMarkImportant }>>
                 <i className="fa fa-exclamation"/>
               </button>
 
