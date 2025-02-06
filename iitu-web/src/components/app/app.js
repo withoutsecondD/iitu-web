bc8db8ba011ff967a0b6b2c8f7c3a9a63e8e3a40
Diyar Z <marbiru15@gmail.com>
2025-02-06 23:58:42 +0500

    step 27 - Added button Add item. Adding element

--- iitu-web/src/components/app/app.js
+++ iitu-web/src/components/app/app.js
@@ -4,6 +4,7 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm  from "../item-add-form";
 
 import './app.css';
 
@@ -42,6 +43,7 @@ export default class App extends Component {
 
                 <TodoList todos={ this.state.todoData}
                           onDeleted = { this.deleteItem }/>
+                <ItemAddForm />
             </div>
         );
     };
